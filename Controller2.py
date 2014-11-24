4d7aa1607b465bad1ee3dc901b97b42a06df5280
nora humpage <NoraH@noras-MacBook-Pro.local>
2014-11-23 20:12:04 -0800

    created controller query for artists based on movementID

--- Controller2.py
+++ Controller2.py
@@ -20,16 +20,58 @@ def utility_processor():
 
     return dict(thumbnail_format=thumbnail_format)
 
-# def login():
-# def signout():
-# def lightbox():
-
 @app.route("/")
 def index():
-    page = render_template("index.html",)
+
+    movements_artists = model.session.query(model.Artist_movement).filter_by(movementId = 436).limit(10).all()
+
+    move_artists = []
+    for artist in movements_artists: 
+        name = artist.artist.name
+        dates = artist.artist.dates
+        numArtwork = len(artist.artist.artworks)
+        artistId = artist.artist.id
+        artworks  = artist.artist.artworks
+        print "**********************************************************************"
+        print name
+        print artworks[0]
+        for art in range(len(artworks)):
+            # found =  False
+            # while thumbnailURL = 0 :
+               if artist.artist.artworks[art].thumbnailURL:
+                   thumbnailURL = artist.artist.artworks[art].thumbnailURL
+                   move_artists.append({"name":name, "id":artistId, "dates":dates, "numArtwork": numArtwork, "thumbnailURL": thumbnailURL})
+                   break
+        print "**********************************************************************"
+
+    print move_artists
+
+
+    page = render_template("index.html")
     return page
 
+   
 
+
+
+
+# def add_details():
+#     movements = model.session.query(model.Movement).all()
+
+#     for movement in movements: 
+#         artistThumbnail = None
+#         numArtwork = 0
+#         for am in movement.artist_movements:
+#             numArtwork += len(am.artist.artworks)
+#             if len(am.artist.artworks)>0:
+#       # if this movement has an artist with an artwork
+#                 for artwork in range(len(am.artist.artworks)):
+#                     # for all the artworks assoc w/ movement
+#                     if am.artist.artworks[artwork].thumbnailURL: 
+#                         artistThumbnail = am.artist.artworks[artwork].thumbnailURL
+
+
+    # move_artists.append({"name":name, "id":artistId, "dates":dates, "numArtwork": numArtwork, "thumbnailURL": thumbnailURL})
 
 
 @app.route("/api/movements", methods=['GET','POST'])
@@ -45,8 +87,6 @@ def load_movments():
         # ls.append( i w/ something done)
     
     return Response(json.dumps(json_movement_objs), mimetype="text/json")
-
-
 
 
 @app.route("/api/artwork",methods=['GET','POST'])
@@ -67,23 +107,28 @@ def load_artwork():
     return Response(json.dumps(artwork_ls), mimetype="text/json")
 
 
-@app.route("/api/artists", methods= ['GET','POST'])
-# get artists for a given movement
-def load_artists():
+# @app.route("/api/artists", methods= ['GET','POST'])
+# # get artists for a given movement
+# def load_artists():
 
-    movement = request.args.get('data')
+#     movement = request.args.get('data')
 
-    movements_artists = model.session.query(model.Artist_movement).filter_by(movementId = movement).all()
+#     movements_artists = model.session.query(model.Artist_movement).filter_by(movementId = movement).limit(5).all()
 
-    move_artists = []
-    for artist in movements_artists: 
-        name = artist.artist.name
-        dates = artist.artist.dates
-        numArtwork = len(artist.artist.artworks)
-        artistId = artist.artist.id
-        move_artists.append({"name":name, "artistId":artistId, "dates":dates, "numArtwork": numArtwork} )
+#     move_artists = []
+#     for artist in movements_artists: 
+#         name = artist.artist.name
+#         dates = artist.artist.dates
+#         numArtwork = len(artist.artist.artworks)
+#         artistId = artist.artist.id
 
-    return Response(json.dumps(move_artists), mimetype="text/json")
+
+
+
+
+#         move_artists.append({"name":name, "id":artistId, "dates":dates, "numArtwork": numArtwork, "thumbnailURL": thumbnailURL})
+
+#     return Response(json.dumps(move_artists), mimetype="text/json")
 
 
 # @app.route("/api/artwork", methods = ['GET','POST'])
