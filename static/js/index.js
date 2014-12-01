810dae33477d979b5adbfc1cb6dd9e3be747b870
nora humpage <NoraH@noras-MacBook-Pro.local>
2014-11-30 23:23:23 -0800

    added intro updated init funct

--- static/js/index.js
+++ static/js/index.js
@@ -204,279 +204,234 @@ function setequalHeight(){
     // $(this).height(window.innerHeight-25);
 }
 
-	function init()
-		{
-			$('li#eraColumn>div>ul>li>img').click(function(evt)
-			{	
+function init()
+	{
+		$('li#eraColumn>div>ul>li>img').click(function(evt)
+		{	
+				$("#columnWrapper>li").css({
+					"position":"absolute",
+					"width":"100%"
+				});
+				$("#logo").css({
+					"width": "200px",
+					"padding-left": "5px",
+					"padding-top":"10px",
+				});
+				$("#intro").remove();
 
-					$("#columnWrapper>li").css({
-						"position":"absolute",
-						"width":"100%"
-					});
-
-					$("#logo").css({
-						"width": "200px",
-						"padding-left": "5px",
-						"padding-top":"10px",
-						"padding-bottom": "2px"
-
-					}); 
-
-					$('#era').css({
-						"display": "block"
-					});
-
-					$('#columnWrapper>li#eraColumn').css({
-						"padding":"0",
-						"height": "auto"
-					});
-
-					$("#columnWrapper,#columnWrapper>li>ul,#columnWrapper>li>div>ul").css({
-						"height": "auto"
-					});
-
-				setequalHeight();
-				var era = $(this).attr("data-era");
-				transitionToMovements(era);
-				
-				addArtwork("/api/movements", {"data":era}, "movementContainer");
+				$('#era').css({
+					"display": "block"
+				});
+				$('#columnWrapper>li#eraColumn').css({
+					"padding":"0",
+					"height": "auto"
+				});
+				$("#columnWrapper,#columnWrapper>li>ul,#columnWrapper>li>div>ul").css({
+					"height": "auto"
+				});
+			setequalHeight();
+			var era = $(this).attr("data-era");
+			transitionToMovements(era);
 			
-			});
-
-			$('#closeMovements,#closeArtists,#closeArtwork').click(function(evt)
-			{
-				closePane($(this).parent());
-			});
-
-	 		$("#movementColumn").on("click", ".fan .overlay", function()
-	 		{
-	 			var movement = $(this).prev().attr("data-id")
-	 			transitionToArtists(movement);
-
-	 			addArtwork("/api/artists",{"data":movement}, "artistContainer");
-	 		});
-
-
-	 		$("#artistColumn").on("click", ".fan .overlay", function()	 			
-	 		{ 
-	 			var artist = $(this).prev().attr("data-id");
-	 			console.log(artist);
- 			 	transitionToArtwork(artist);
- 			 	addArtwork("/api/artwork",{data:$(this).prev().attr("data-id")}, "artworkContainer");
-	 		});
- 	
-	 	}
-
-
-	function closePane(pane)
-		{
-			var paneToExpand = pane.prev();
-			var paneToExpand2 = pane;
-			// console.log(paneToExpand.attr("id"));
-
-			pane.find(".fan").empty();
-
-			switch (paneToExpand.attr("id"))
-			{
-				case ("eraColumn"):
-					transitionToEras(pane);
-					break;
-				default:
-					transitionToPrev(paneToExpand2);
-					transitionToPrev(paneToExpand);
-			}
-		}
-
-	function transitionToEras(pane)
-		{
-			$("#eraColumn").animate({
-				"width": "100%"
-			},250,function()
-			{
-				movementsVisable = false;
-			});
-
-			pane.children("div").animate({
-				"marginLeft": "20%"
-			});
-
-			pane.animate({
-				"width": "100%"
-			},250,function()
-			{
-				
-			});
-		}
-
-	function transitionToPrev(paneToExpand)
-		{
-			console.log(paneToExpand.attr("id"));
-			paneToExpand.prev().animate({
-				"width": "20%"
-			},250,function()
-			{
-				
-			});
-
-			paneToExpand.prev().find(".imgContainer>img").css({
-				"width": "70%"
-			});
-
-			paneToExpand.children("div").animate({
-				"marginLeft": "20%"
-			});
-
-			paneToExpand.animate({
-				"width": "100%"
-			},250,function()
-			{
-				
-			});
-
-			paneToExpand.find(".imgContainer").css({
-				"margin-left":"5%",
-			    "margin-right":"17%",
-			    "margin-top":"5%",
-
-			});
-
-			paneToExpand.find(".imgContainer>img").css({
-				"width": "150%"
-			});
-
-			paneToExpand.find(".overlay").css({
-				"width": "156%",
-				"left": "0%"
-			});
-
-			paneToExpand.find(".overlay>p").css({
-				"font-size":"2.5em",
-			});
-
-			paneToExpand.find(".overlay>div").css({
-				"font-size":"1.5em",
-			});
-		}
-
-	function transitionToMovements(era)
-		{
-			$("#eraColumn").animate({
-				"width": "20%"
-			},250,function()
-			{
-				movementsVisable = true;
-			});
-
-			$("#columnWrapper>li#movementColumn").css({
-				"display" : "block"
-			});
-
-			$("#columnWrapper>li#artistColumn").css({
-				"display" : "block"
-			});
-
-			$("#columnWrapper>li#artworkColumn").css({
-				"display" : "block"
-			});
-
-
-
-
-
-
-			$(".fan").css({
-				"overflow" : "scroll"
-			});
-
-			$("#closeArtwork>img ").css({
-				"display" : "block"
-			})
-			
-		}
-
-	function transitionToArtists(movement)
-		{
-			$("#eraColumn").animate({
-				"width": "0%"
-			},250,function()
-			{
-				
-			});
-
-			$("#movementColumn>div").animate({
-				"marginLeft": "0%"
-			});
-
-			$("#movementColumn").animate({
-				"width": "20%"
-			},250,function()
-			{
-			});
-
-			$("#movementColumn .imgContainer").css({
-				"margin-top": "5%",
-				
-			});
-
-			$("#movementColumn .imgContainer>img").css({
-				"width": "70%",
-				
-			});
-
-			$("#movementColumn .overlay").css({
-				"width": "80%",
-				"left":"10%",
-			});
-
-			$("#movementColumn .overlay>*").css({
-				"font-size":"18px"
-			});
-
-		}
-
+			addArtwork("/api/movements", {"data":era}, "movementContainer");
 		
-	function transitionToArtwork(artist)
+		});
+		$('#closeMovements,#closeArtists,#closeArtwork').click(function(evt)
 		{
-			$("#movementColumn").animate({
-				"width" : "0%"
-			},250,function()
-			{
-				
-			});
+			closePane($(this).parent());
+		});
+ 		$("#movementColumn").on("click", ".fan .overlay", function()
+ 		{
+ 			var movement = $(this).prev().attr("data-id")
+ 			transitionToArtists(movement);
+ 			addArtwork("/api/artists",{"data":movement}, "artistContainer");
+ 		});
+ 		$("#artistColumn").on("click", ".fan .overlay", function()	 			
+ 		{ 
+ 			var artist = $(this).prev().attr("data-id");
+ 			console.log(artist);
+ 		 	transitionToArtwork(artist);
+ 		 	addArtwork("/api/artwork",{data:$(this).prev().attr("data-id")}, "artworkContainer");
+ 		});
+ 
+ 	}
 
-			$("#artistColumn>div").animate({
-				"marginLeft" : "0%"
-		
-			});
+function closePane(pane){
 
-			$("#artistColumn").animate({
-				"width" : "20%"
-			},250,function()
-			{
-
-			});
-
-			$("#artistColumn .imgContainer").css({
-				"margin-top": "5%",
-				
-			});
-
-			$("#artistColumn .imgContainer>img").css({
-				"width": "70%",
-				
-			});
-
-			$("#artistColumn .overlay").css({
-				"width": "80%",
-				"left":"10%"
-			});
-
-			$("#artistColumn .overlay>*").css({
-				"font-size":"18px"
-			});
-				
+		var paneToExpand = pane.prev();
+		var paneToExpand2 = pane;
+		// console.log(paneToExpand.attr("id"));
+		pane.find(".fan").empty();
+		switch (paneToExpand.attr("id"))
+		{
+			case ("eraColumn"):
+				transitionToEras(pane);
+				break;
+			default:
+				transitionToPrev(paneToExpand2);
+				transitionToPrev(paneToExpand);
 		}
+}
+
+function updateNav(container, data-name){
+
+	
+}
+
+
+function transitionToEras(pane){
+
+		$("#eraColumn").animate({
+			"width": "100%"
+		},250,function()
+		{
+			movementsVisable = false;
+		});
+		pane.children("div").animate({
+			"marginLeft": "20%"
+		});
+		pane.animate({
+			"width": "100%"
+		},250,function()
+		{
 			
+		});
+}
+
+function transitionToPrev(paneToExpand){
+
+		console.log(paneToExpand.attr("id"));
+		paneToExpand.prev().animate({
+			"width": "20%"
+		},250,function()
+		{
+			
+		});
+		paneToExpand.prev().find(".imgContainer>img").css({
+			"width": "70%"
+		});
+		paneToExpand.children("div").animate({
+			"marginLeft": "20%"
+		});
+		paneToExpand.animate({
+			"width": "100%"
+		},250,function()
+		{
+			
+		});
+		paneToExpand.find(".imgContainer").css({
+			"margin-left":"5%",
+		    "margin-right":"17%",
+		    "margin-top":"5%",
+		});
+		paneToExpand.find(".imgContainer>img").css({
+			"width": "150%"
+		});
+		paneToExpand.find(".overlay").css({
+			"width": "156%",
+			"left": "0%"
+		});
+		paneToExpand.find(".overlay>p").css({
+			"font-size":"2.5em",
+		});
+		paneToExpand.find(".overlay>div").css({
+			"font-size":"1.5em",
+		});
+}
+
+function transitionToMovements(era){
+
+		$("#eraColumn").animate({
+			"width": "20%"
+		},250,function()
+		{
+			movementsVisable = true;
+		});
+		$("#columnWrapper>li#movementColumn").css({
+			"display" : "block"
+		});
+		$("#columnWrapper>li#artistColumn").css({
+			"display" : "block"
+		});
+		$("#columnWrapper>li#artworkColumn").css({
+			"display" : "block"
+		});
+		$(".fan").css({
+			"overflow" : "scroll"
+		});
+		$("#closeArtwork>img ").css({
+			"display" : "block"
+		})
+}
+
+function transitionToArtists(movement){
+
+		$("#eraColumn").animate({
+			"width": "0%"
+		},250,function()
+		{
+			
+		});
+		$("#movementColumn>div").animate({
+			"marginLeft": "0%"
+		});
+		$("#movementColumn").animate({
+			"width": "20%"
+		},250,function()
+		{
+		});
+		$("#movementColumn .imgContainer").css({
+			"margin-top": "5%",
+			
+		});
+		$("#movementColumn .imgContainer>img").css({
+			"width": "70%",
+			
+		});
+		$("#movementColumn .overlay").css({
+			"width": "80%",
+			"left":"10%",
+		});
+		$("#movementColumn .overlay>*").css({
+			"font-size":"18px"
+		});
+}
+	
+function transitionToArtwork(artist){
+
+		$("#movementColumn").animate({
+			"width" : "0%"
+		},250,function()
+		{
+			
+		});
+		$("#artistColumn>div").animate({
+			"marginLeft" : "0%"
+	
+		});
+		$("#artistColumn").animate({
+			"width" : "20%"
+		},250,function()
+		{
+		});
+		$("#artistColumn .imgContainer").css({
+			"margin-top": "5%",
+			
+		});
+		$("#artistColumn .imgContainer>img").css({
+			"width": "70%",
+			
+		});
+		$("#artistColumn .overlay").css({
+			"width": "80%",
+			"left":"10%"
+		});
+		$("#artistColumn .overlay>*").css({
+			"font-size":"18px"
+		});
+			
+}
+		
 
 
 
